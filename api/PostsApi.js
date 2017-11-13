fdf06d53cc1d2942c60e2df16dd4588b74aaecdc
Vlad <vieira83@gmail.com>
2017-11-13 09:42:17 -0500

    fixed apifetch posts

--- api/PostsApi.js
+++ api/PostsApi.js
@@ -3,9 +3,18 @@ export default  class Posts {
 
   static getPosts() {
     return fetch('http://localhost:9001/posts').then(response => {
-      const data = response.json();
-      return data.length > 10 ? data.splice(0, 10) : data;
+      const posts = response.json();
+      return posts.length > 10 ? posts.splice(0, 10) : posts;
     }).catch(error => {
+      return error;
+    });
+  }
+
+  static get(id) {
+    return fetch('http://localhost:9001/posts')
+    .then(response => response.json())
+    .then(posts =>  posts.filter(post => post.id === parseInt(id,10))[0])
+    .catch(error => {
       return error;
     });
   }
--- src/app/home/posts/ViewPost.js
+++ src/app/home/posts/ViewPost.js
@@ -2,18 +2,32 @@ import React from "react";
 
 // Render static HTML:
 import __html from "./list.html";
+import PostsApi from "../../../../api/PostsApi";
 
 export class ViewPost extends React.Component {
   constructor(props) {
     super(props);
+      this.state = {post:''};
   }
   componentDidMount() {
-    console.log(this.props);
+    PostsApi.get(this.props.params.id).then(post => {
+      this.setState({post});
+      debugger;
+      console.log(post);
+    });
   }
   render() {
     return <div>
         <div dangerouslySetInnerHTML={{__html}} />
-
+          <h2>{this.state.post.title}</h2>
+          <hr/>
+          <h3>{this.state.post.description} </h3>
+          <div>{this.state.post.content}</div>
+          <div>
+          <br/>
+            <span>Date Published: {this.state.post.publish_date} </span><br/>
+            <span>Author: {this.state.post.author} </span>
+          </div>
     </div>
   }
 }
